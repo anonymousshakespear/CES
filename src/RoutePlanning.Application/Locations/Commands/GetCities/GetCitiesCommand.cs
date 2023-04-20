90f604d03bb0f8b52bb8fe6cc9a29512e69d0f3c
Michał Dziekan <dean@netcompany.com>
2023-04-20 22:33:32 +0200

    added cityconfiguration and citiescommand

---
+++ src/RoutePlanning.Application/Locations/Commands/GetCities/GetCitiesCommand.cs
@@ -0,0 +1,5 @@
+﻿using Netcompany.Net.Cqs.Commands;
+
+namespace RoutePlanning.Application.Locations.Commands.GetCities;
+
+public sealed record GetCitiesCommand(string name, string status) : ICommand;

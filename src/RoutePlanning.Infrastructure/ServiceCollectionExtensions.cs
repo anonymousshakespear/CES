5b092c552da34e407c1de4a567673382ca63344a
ncdmz\lmp <lmp@netcompany.com>
2023-04-20 18:07:07 +0700

    update

--- src/RoutePlanning.Infrastructure/ServiceCollectionExtensions.cs
+++ src/RoutePlanning.Infrastructure/ServiceCollectionExtensions.cs
@@ -3,21 +3,19 @@ using RoutePlanning.Infrastructure.Database;
 using Netcompany.Net.DomainDrivenDesign;
 using Netcompany.Net.UnitOfWork;
 using Netcompany.Net.UnitOfWork.AmbientTransactions;
-using Microsoft.EntityFrameworkCore.Diagnostics;
-using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace RoutePlanning.Infrastructure;
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddRoutePlanningInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddRoutePlanningInfrastructure(this IServiceCollection services, IConfiguration config)
     {
-        var keepAliveConnection = new SqliteConnection("DataSource=:memory:");
-        keepAliveConnection.Open();
         services.AddDbContext<RoutePlanningDatabaseContext>(builder =>
         {
-            builder.UseSqlite(keepAliveConnection);
-            builder.ConfigureWarnings(x => x.Ignore(RelationalEventId.AmbientTransactionWarning));
+            //builder.UseSqlite(keepAliveConnection);
+            //builder.ConfigureWarnings(x => x.Ignore(RelationalEventId.AmbientTransactionWarning));
+            builder.UseSqlServer(config.GetConnectionString("Default"));
         });
 
         services.AddDomainDrivenDesign(options => options.UseDbContext<RoutePlanningDatabaseContext>());

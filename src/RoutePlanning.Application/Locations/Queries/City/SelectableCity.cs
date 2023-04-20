c668ff031821125d8d1f6b8e5c99bf9e80446936
Michał Dziekan <dean@netcompany.com>
2023-04-20 20:56:41 +0200

    selectableCityQuery added

---
+++ src/RoutePlanning.Application/Locations/Queries/City/SelectableCity.cs
@@ -0,0 +1,4 @@
+﻿
+namespace RoutePlanning.Application.Locations.Queries.City;
+
+public sealed record SelectableCity(Domain.Locations.Models.City.EntityId cityID, string name, bool status, DateTime rowVersion);
---
+++ src/RoutePlanning.Application/Locations/Queries/City/SelectableCityQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Netcompany.Net.Cqs.Queries;
+
+namespace RoutePlanning.Application.Locations.Queries.City;
+
+public class SelectableCityQuery : IQuery<IReadOnlyList<SelectableCity>>
+{
+}

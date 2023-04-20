f7b86a7eee37e3a798b3e36a89215cf6a0d00907
HoaDoDuc <ddhoa@netcompany.com>
2023-04-20 15:50:20 +0200

    Integrate front end into project

--- src/RoutePlanning.Client.Web/DatabaseInitialization.cs
+++ src/RoutePlanning.Client.Web/DatabaseInitialization.cs
@@ -1,6 +1,6 @@
 ﻿using Netcompany.Net.UnitOfWork;
-using RoutePlanning.Domain.Locations;
-using RoutePlanning.Domain.Users;
+//using RoutePlanning.Domain.Locations;
+//using RoutePlanning.Domain.Users;
 using RoutePlanning.Infrastructure.Database;
 
 namespace RoutePlanning.Client.Web;
@@ -24,38 +24,38 @@ public static class DatabaseInitialization
         }
     }
 
-    private static async Task SeedLocationsAndRoutes(RoutePlanningDatabaseContext context)
-    {
-        var berlin = new Location("Berlin");
-        await context.AddAsync(berlin);
+    //private static async Task SeedLocationsAndRoutes(RoutePlanningDatabaseContext context)
+    //{
+    //    var berlin = new Location("Berlin");
+    //    await context.AddAsync(berlin);
 
-        var copenhagen = new Location("Copenhagen");
-        await context.AddAsync(copenhagen);
+    //    var copenhagen = new Location("Copenhagen");
+    //    await context.AddAsync(copenhagen);
 
-        var paris = new Location("Paris");
-        await context.AddAsync(paris);
+    //    var paris = new Location("Paris");
+    //    await context.AddAsync(paris);
 
-        var warsaw = new Location("Warsaw");
-        await context.AddAsync(warsaw);
+    //    var warsaw = new Location("Warsaw");
+    //    await context.AddAsync(warsaw);
 
-        CreateTwoWayConnection(berlin, warsaw, 573);
-        CreateTwoWayConnection(berlin, copenhagen, 763);
-        CreateTwoWayConnection(berlin, paris, 1054);
-        CreateTwoWayConnection(copenhagen, paris, 1362);
-    }
+    //    CreateTwoWayConnection(berlin, warsaw, 573);
+    //    CreateTwoWayConnection(berlin, copenhagen, 763);
+    //    CreateTwoWayConnection(berlin, paris, 1054);
+    //    CreateTwoWayConnection(copenhagen, paris, 1362);
+    //}
 
-    private static async Task SeedUsers(RoutePlanningDatabaseContext context)
-    {
-        var alice = new User("alice", User.ComputePasswordHash("alice123!"));
-        await context.AddAsync(alice);
+    //private static async Task SeedUsers(RoutePlanningDatabaseContext context)
+    //{
+    //    var alice = new User("alice", User.ComputePasswordHash("alice123!"));
+    //    await context.AddAsync(alice);
 
-        var bob = new User("bob", User.ComputePasswordHash("!CapableStudentCries25"));
-        await context.AddAsync(bob);
-    }
+    //    var bob = new User("bob", User.ComputePasswordHash("!CapableStudentCries25"));
+    //    await context.AddAsync(bob);
+    //}
 
-    private static void CreateTwoWayConnection(Location locationA, Location locationB, int distance)
-    {
-        locationA.AddConnection(locationB, distance);
-        locationB.AddConnection(locationA, distance);
-    }
+    //private static void CreateTwoWayConnection(Location locationA, Location locationB, int distance)
+    //{
+    //    locationA.AddConnection(locationB, distance);
+    //    locationB.AddConnection(locationA, distance);
+    //}
 }

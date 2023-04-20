72c7cfe44213054265d91b19c46790f692420016
Michał Dziekan <dean@netcompany.com>
2023-04-20 13:10:15 +0200

    test structure

---
+++ tests/RoutePlanning.Client.Web.ApiTests/ServicesTests/RoutingServiceHelperTest.cs
@@ -0,0 +1,15 @@
+﻿
+namespace RoutePlanning.Client.Web.ApiTests.BackEndTests;
+public class RoutingServiceHelperTest
+{
+    public void BuildGraphTest()
+    {
+        //Should return a weighted graph that will be used for the calculation of cost and time of shipment routes
+    }
+
+    public void OptimisePathTest()
+    {
+        // Should return the best path between two points
+    }
+
+}
---
+++ tests/RoutePlanning.Client.Web.ApiTests/ServicesTests/RoutingServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace RoutePlanning.Client.Web.ApiTests.BackEndTests;
+public class RoutingServiceTest
+{
+    public void FindShortestRouteTest()
+    {
+        // Should return the shortest route between two shipment locations
+    }
+    public void FindPreferableRouteTest()
+    {
+        // Should return a route that has the biggest possible part of it made by East India Trading Co
+    }
+
+    public void CalculateRouteCostTest()
+    {
+        // Should return expected cost of a route
+    }
+}

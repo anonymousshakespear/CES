bf008024f6f85d30a2a5fea234ddd71743b5ebc6
Alicja Olejnik <olnk@netcompany.com>
2023-04-20 18:54:24 +0200

    api calls to OA

--- src/RoutePlanning.Client.Web/Api/RouteController.cs
+++ src/RoutePlanning.Client.Web/Api/RouteController.cs
@@ -1,8 +1,12 @@
-﻿using MediatR;
+﻿using System.Net.Http.Headers;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 using RoutePlanning.Application.Locations.Commands.CreateTwoWayConnection;
 using RoutePlanning.Client.Web.Authorization;
+using RoutePlanning.Client.Web.Shared;
 
 namespace RoutePlanning.Client.Web.Api;
 
@@ -12,21 +16,201 @@ namespace RoutePlanning.Client.Web.Api;
 public sealed class RoutesController : ControllerBase
 {
     private readonly IMediator _mediator;
-
+ 
     public RoutesController(IMediator mediator)
     {
         _mediator = mediator;
     }
 
     [HttpGet("[action]")]
-    public Task<string> HelloWorld()
+    public async Task<string> HelloWorld()
     {
-        return Task.FromResult("Hello World!");
+        try
+        {
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                using (var response = await httpClient
+                    .GetAsync(
+                        "http://www.google.com")
+                )
+                {
+                    var apiResponse = await response.Content.ReadAsStringAsync();
+                }
+            }
+
+            return await Task.FromResult("Hello World!").ConfigureAwait(false);
+        }
+        catch
+        {
+            throw new Exception("a");
+        }
     }
 
     [HttpPost("[action]")]
-    public async Task AddTwoWayConnection(CreateTwoWayConnectionCommand command)
+    public async Task<string> getSegmentFromOceania(GetSegmentRequestDto getSegmentRequestDto)
     {
-        await _mediator.Send(command);
+        try
+        {
+            var myContent = JsonConvert.SerializeObject(getSegmentRequestDto);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+            var byteContent = new ByteArrayContent(buffer);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                using (var response = await httpClient
+                    .PostAsync(
+                        "https://wa-eit-eit1.azurewebsites.net/api/GetSegment",
+                        byteContent
+                        )
+                )
+                {
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("Searching unsuccessful");
+                    }
+
+                    var finalData = await response.Content.ReadAsStringAsync();
+
+                    var _dataResponse = JToken.Parse(JsonConvert.SerializeObject(finalData));
+
+                    var userResult = new GetSegmentResponseDto()
+                    {
+                        cost = Convert.ToDouble(_dataResponse["Cost"].ToString()),
+                        time = Convert.ToDouble(_dataResponse["Time"].ToString()),
+                    };
+                }
+            }
+
+            return await Task.FromResult("Hello World!").ConfigureAwait(false);
+        }
+        catch
+        {
+            throw new Exception("a");
+        }
+    }    
+    
+    [HttpPost("[action]")]
+    public async Task<string> bookSegmentFromOceania(BookSegmentRequestDto bookSegmentRequestDto)
+    {
+        try
+        {
+            var myContent = JsonConvert.SerializeObject(bookSegmentRequestDto);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+            var byteContent = new ByteArrayContent(buffer);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Clear();
+                httpClient.DefaultRequestHeaders.Add("Issuer", "EIT");
+                httpClient.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                using (var response = await httpClient
+                    .PostAsync(
+                        "https://wa-eit-eit1.azurewebsites.net/api/BookSegment",
+                        byteContent
+                        )
+                )
+                {
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("Booking unsuccessful");
+                    }
+
+                    var finalData = await response.Content.ReadAsStringAsync();
+                    var _dataResponse = JToken.Parse(JsonConvert.SerializeObject(finalData));
+
+                    var userResult = new BookSegmentResponseDto()
+                    {
+                        confirmationID = _dataResponse["ConfirmationID"].ToString(),
+                        cost = Convert.ToDouble(_dataResponse["Cost"].ToString()),
+                        time = Convert.ToDouble(_dataResponse["Time"].ToString()),
+                    };
+                }
+            }
+
+            return await Task.FromResult("Hello World!").ConfigureAwait(false);
+        }
+        catch
+        {
+            throw new Exception("a");
+        }
+    }
+
+    [HttpPost("[action]")]
+    public async Task<string> deleteSegmentFromOceania(string confirmationId)
+    {
+        try
+        {
+            var myContent = JsonConvert.SerializeObject(new DeleteSegmentRequestDto(confirmationId));
+            var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+            var byteContent = new ByteArrayContent(buffer);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Clear();
+                httpClient.DefaultRequestHeaders.Add("Issuer", "EIT");
+                httpClient.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                using (var response = await httpClient
+                    .PostAsync(
+                        "https://wa-eit-eit1.azurewebsites.net/api/DeleteSegment",
+                        byteContent
+                        )
+                )
+                {
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("Deleting unsuccessful");
+                    }
+                }
+            }
+
+            return await Task.FromResult("Hello World!").ConfigureAwait(false);
+        }
+        catch
+        {
+            throw new Exception("a");
+        }
+    }
+
+    [HttpPost("[action]")]
+    public async Task<string> AddTwoWayConnection(CreateTwoWayConnectionCommand command)
+    {
+        try
+        {
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                using (var response = await httpClient
+                    .GetAsync(
+                        "http://www.google.com")
+                )
+                {
+                    var apiResponse = await response.Content.ReadAsStringAsync();
+                }
+            }
+
+            await _mediator.Send(command);
+            return await Task.FromResult("Hello World!").ConfigureAwait(false);
+        }
+        catch
+        {
+            return await Task.FromResult("Hello Worldwdwdwdwdwd!").ConfigureAwait(false);
+        }
     }
 }
---
+++ src/RoutePlanning.Client.Web/Shared/BookSegmentRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RoutePlanning.Client.Web.Shared;
+
+public class BookSegmentRequestDto : GetSegmentRequestDto
+{
+    public DateTime time = DateTime.Now;
+    public string packageId = "";
+
+    public BookSegmentRequestDto() { }
+}
